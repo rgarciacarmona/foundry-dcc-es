c8a9dc2ae5295cc7d23ab6f6883aada05e00db06
LAPTOP-AKJKV4TG\danie <danielvarelagil@gmail.com>
2023-08-20 18:17:09 +0200

    Replace faulty google_translator_new by translators module

--- utils/parseEn.py
+++ utils/parseEn.py
@@ -2,22 +2,20 @@ import json
 import fileinput
 import pandas
 import re
-from google_trans_new import google_translator
+import translators as ts
 
 # requires 
 # pandas
-# googletrans 4.0.0rc1, had to tweak it to fix gender specific translation issues
-# https://github.com/ssut/py-googletrans/issues/260#issuecomment-752342998
+# translators
 
 dict = list();
-translator = google_translator(timeout=10)
 
 def translate(en):
    matches = re.findall(r'{(.+?)}',en)
    translated = "";
    if not matches:
        try:
-           translated = translator.translate(en, src='en', dest='es').text
+           translated = ts.server.google(en, from_language='en', to_language='es')
        except:
            translated = en
    else:
@@ -29,7 +27,7 @@ def translate(en):
            chunk = en[last:matchPos]
            if (not chunk.isspace()) and (len(chunk) > 0):
                try:
-                   translated += translator.translate(chunk, src='en', dest='es').text
+                   translated += ts.server.google(en, from_language='en', to_language='es')
                except:
                    translated += chunk 
            else:
